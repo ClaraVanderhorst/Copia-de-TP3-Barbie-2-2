839f664a2875c73db84b41a6a4614c666b93dac8
ClaraVanderhorst <c.vanderhorstskou@mail.utdt.edu>
2024-12-03 18:49:36 -0300

    First commit

---
+++ src/components/Marcas.svelte
@@ -0,0 +1,133 @@
+<script>
+  export let goHome;
+</script>
+
+<main>
+
+  <div class="text-container">
+    <h1>Colecciones Iconicas</h1>
+    <p>Barbie hizo muchas colaboraciones con distintas marcas que fueron muy exitosas. Cuando Barbie se une con las grandes marcas, el resultado es un fenómeno global.</p>
+    <img src="/images/estrella1.png" alt="estrella1" class="estrella1" width="40px" style="top:25%; left: 16%;">
+    <img src="/images/estrella2.png" alt="estrella2" class="estrella2" width="40px" style="top:87%; right: 16%;">
+  
+  </div>
+
+<div class="button-container">
+  <button on:click={() => goHome("home")}>
+    <img src="/images/casacuartos/casamattel.png" alt="Casa" class="casa-image">
+</button>
+</div>
+<div class="flourish">
+<iframe src='https://flo.uri.sh/story/2739152/embed' title='Interactive or visual content' class='flourish-embed-iframe' frameborder='0' scrolling='no' style='width:100%;height:600px;' sandbox='allow-same-origin allow-forms allow-scripts allow-downloads allow-popups allow-popups-to-escape-sandbox allow-top-navigation-by-user-activation'></iframe><div style='width:100%!;margin-top:4px!important;text-align:right!important;'><a class='flourish-credit' href='https://public.flourish.studio/story/2739152/?utm_source=embed&utm_campaign=story/2739152' target='_top' style='text-decoration:none!important'><img alt='Made with Flourish' src='https://public.flourish.studio/resources/made_with_flourish.svg' style='width:105px!important;height:16px!important;border:none!important;margin:0!important;'> </a></div>
+</div>
+</main>
+
+<style>
+
+.estrella1{
+    position: absolute;
+    animation: twinkle 1s infinite alternate;
+    top: calc(var(--i) * 5%);
+    left: calc(var(--i) * 7%);
+    opacity: 0.5;
+  }
+
+  @keyframes twinkle {
+    0% {
+      opacity: 0;
+      transform: scale(0.4);
+    }
+    100% {
+      opacity: 1;
+      transform: scale(1.2);
+    }
+  }
+
+  .estrella2{
+    position: absolute;
+    animation: twinkle 1s infinite alternate;
+    top: calc(var(--i) * 5%);
+    left: calc(var(--i) * 7%);
+    opacity: 0.5;
+  }
+
+  @keyframes twinkle {
+    0% {
+      opacity: 0;
+      transform: scale(0.6);
+    }
+    100% {
+      opacity: 1;
+      transform: scale(1.2);
+    }
+  }
+
+.text-container {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    flex-direction: column;
+    margin-bottom: 10px;
+    text-align: center;
+    padding: 0 10px;
+    position: relative;
+    background-color: #FFE3F5;
+  }
+
+  .text-container h1 {
+    padding-top: 20px;
+    font-family: "Bartex";
+    font-size: clamp(3rem, 8vw, 7em); /* Escala entre 3rem y 7em */
+    margin: 0;
+    color: #ffffff;
+    font-weight: 600;
+    text-shadow: 3px 5px 0px #E81D8D;
+  }
+
+  .text-container p {
+    font-family: "Helvetica";
+    font-size: 1em;
+    margin: 20px auto;
+    max-width: 50%; /* Ajusta el ancho para pantallas pequeñas */
+    color: #710A43;
+    text-align: center;
+  }
+
+  .button-container {
+  background-color: #ffe3f5;
+  width: 100%;
+  position: sticky;
+  top: 0;
+  z-index: 10;
+  display: flex; /* Flexbox para centrar contenido */
+  justify-content: center; /* Centra horizontalmente */
+  align-items: center; /* Centra verticalmente */
+  height: 150px; /* Ajusta la altura del contenedor */
+}
+
+/* Botón */
+button {
+  cursor: pointer;
+  background-color: transparent; /* Fondo transparente */
+  border: none; /* Sin bordes */
+  padding: 0; /* Sin padding para ajustar al tamaño de la imagen */
+  display: flex; /* Flexbox para centrar la imagen dentro del botón */
+  justify-content: center;
+  align-items: center;
+  width: 100px; /* Ancho del botón */
+  height: 100px; /* Alto del botón */
+}
+
+/* Imagen de la casa */
+.casa-image {
+  max-width: 100%;
+  max-height: 100%;
+  width: auto; /* Mantiene proporciones */
+  height: auto; /* Mantiene proporciones */
+}
+
+
+
+main{background-color: #FFE3F5;}
+
+</style>
---
+++ src/components/Presupuesto.svelte
@@ -0,0 +1,202 @@
+<script>
+  // Función para inicializar el componente de Flourish y tomar el script que nos permite hacer el scrolly gratis!
+  import { onMount } from 'svelte';
+
+  
+
+  // Array para guardar la data de cada slide. Cambiar cada uno con el epígrafe específico para cada slide.
+  const slides = [
+    "Texto slide 1. Fusce quis augue et tortor interdum bibendum. Nam dolor elit",
+    "Texto slide 2. Fusce quis augue et tortor interdum bibendum. Nam dolor elit",
+    "Texto slide 3. Fusce quis augue et tortor interdum bibendum. Nam dolor elit",
+    "Texto slide 4. Fusce quis augue et tortor interdum bibendum. Nam dolor elit",
+    "Texto slide 5. Fusce quis augue et tortor interdum bibendum. Nam dolor elit",
+    "Texto slide 6. Fusce quis augue et tortor interdum bibendum. Nam dolor elit",
+    "Texto slide 7. Fusce quis augue et tortor interdum bibendum. Nam dolor elit",
+    "Texto slide 8. Fusce quis augue et tortor interdum bibendum. Nam dolor elit",
+    "Texto slide 9. Fusce quis augue et tortor interdum bibendum. Nam dolor elit",
+    "Texto slide 10. Fusce quis augue et tortor interdum bibendum. Nam dolor elit",
+    "Texto slide 11. Fusce quis augue et tortor interdum bibendum. Nam dolor elit",
+    "Texto slide 12. Fusce quis augue et tortor interdum bibendum. Nam dolor elit",
+    "Texto slide 13. Fusce quis augue et tortor interdum bibendum. Nam dolor elit",
+    "Texto slide 14. Fusce quis augue et tortor interdum bibendum. Nam dolor elit",
+    "Texto slide 15. Fusce quis augue et tortor interdum bibendum. Nam dolor elit",
+    "Texto slide 16. Fusce quis augue et tortor interdum bibendum. Nam dolor elit",
+    "Texto slide 17. Fusce quis augue et tortor interdum bibendum. Nam dolor elit",
+    "Texto slide 18. Fusce quis augue et tortor interdum bibendum. Nam dolor elit",
+    "Texto slide 19. Fusce quis augue et tortor interdum bibendum. Nam dolor elit"
+  ]
+
+  function loadFlourishScrolly() {
+    const script = document.createElement('script')
+    script.src = "https://cdn.flourish.rocks/flourish-scrolly-v3.1.0.min.js"
+    script.type = "text/javascript"
+    script.onload = () => initFlourishScrolly()
+    document.body.appendChild(script)
+  }
+
+
+  onMount(() => {
+    loadFlourishScrolly()
+  });
+  export let goHome;
+</script>
+
+<main>
+
+  <div class="text-container">
+    <h1>Marcas</h1>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Numquam officia itaque doloribus quae quis dolorem impedit, voluptates nostrum. Repudiandae adipisci magni ipsam quisquam explicabo excepturi quia voluptas, natus magnam vel.</p>
+  </div>
+
+<div class="button-container">
+  <button on:click={() => goHome("home")}>
+    <img src="/images/casacuartos/casamattel.png" alt="Casa" class="casa-image">
+</button>
+</div>
+
+<section id="header">
+  <h1 id="main_title">
+    Uno por uno, quiénes son los jugadores con más chances de jugar en el Mundial
+  </h1>
+</section>
+
+<div class="texto_container">
+  <p class="texto_parrafo">
+    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam eget mi eu nulla porta fringilla. Curabitur quis tincidunt purus. Duis nec neque et augue dictum blandit a eget neque. Phasellus tempor, metus et finibus pretium, eros est sagittis orci, vitae pellentesque nisl lectus et velit. Praesent fringilla mauris vitae magna sollicitudin, sit amet tincidunt dolor facilisis. Fusce quis augue et tortor interdum bibendum. Nam dolor elit, posuere nec tortor non, molestie congue justo. Praesent nisl arcu, consectetur sed hendrerit eget, interdum ac nisi. Vestibulum tempor mattis purus. Aliquam tempus neque sed velit convallis tempor. Cras sodales nunc lorem. Nulla pharetra malesuada consequat. Nulla elementum odio non mauris mollis suscipit. Praesent consectetur nunc eget ex varius blandit. Sed id facilisis tellus.
+  </p>
+</div>
+
+<div class="texto_container">
+  <h2 class="subtitulo">Los posibles elegidos de Scaloni</h2>
+</div>
+
+<!-- Contenedor de la story de flourish (scrolly)-->
+<div id="my-wrapper">
+  <!-- Reemplazar el ID de jeemplo por el de la story propia -->
+  <div class="flourish-embed" data-src="story/2041907" data-url="https://flo.uri.sh/story/2041907/embed" data-height="100vh">
+    
+    <!-- <script src="https://public.flourish.studio/resources/embed.js"></script> -->
+  </div>
+
+  <!-- Iteramos sobre las distintas slides del componente de Flourish -->
+  {#each slides as slide, index}
+    <p>
+      {@html slide}
+      <!-- svelte-ignore a11y-missing-content -->
+      <a href={"#story/2041907/slide-" + (index + 1)}></a>
+    </p>
+  {/each}
+</div>
+
+<div class="texto_container">
+  <p class="texto_parrafo">
+    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam eget mi eu nulla porta fringilla. Curabitur quis tincidunt purus. Duis nec neque et augue dictum blandit a eget neque. Phasellus tempor, metus et finibus pretium, eros est sagittis orci, vitae pellentesque nisl lectus et velit. Praesent fringilla mauris vitae magna sollicitudin, sit amet tincidunt dolor facilisis. Fusce quis augue et tortor interdum bibendum. Nam dolor elit, posuere nec tortor non, molestie congue justo. Praesent nisl arcu, consectetur sed hendrerit eget, interdum ac nisi. Vestibulum tempor mattis purus. Aliquam tempus neque sed velit convallis tempor. Cras sodales nunc lorem. Nulla pharetra malesuada consequat. Nulla elementum odio non mauris mollis suscipit. Praesent consectetur nunc eget ex varius blandit. Sed id facilisis tellus.
+  </p>
+</div>
+
+</main>
+
+
+
+<style>
+
+main{background-color: #ffffff;}
+
+.text-container {
+    width: 100%;
+    text-align: center;
+    padding: 20px 0;
+   
+}
+  
+.text-container h1 {
+    font-family: "Bartex";
+    font-size: 7em;
+    margin: 0;
+    color: #ffffff;
+    font-weight: 600;
+    text-shadow: 3px 5px 0px #E81D8D;
+}
+  
+.text-container p {
+    font-family: "Helvetica";
+    font-size: 1em;
+    margin: 20px 200px;
+    color: #710A43;
+}
+
+.button-container {
+  background-color: #ffffff;
+  width: 100%;
+  position: sticky;
+  top: 0;
+  z-index: 10;
+  display: flex; /* Flexbox para centrar contenido */
+  justify-content: center; /* Centra horizontalmente */
+  align-items: center; /* Centra verticalmente */
+  height: 200px; /* Ajusta la altura del contenedor */
+}
+
+/* Botón */
+button {
+  cursor: pointer;
+  background-color: transparent; /* Fondo transparente */
+  border: none; /* Sin bordes */
+  padding: 0; /* Sin padding para ajustar al tamaño de la imagen */
+  display: flex; /* Flexbox para centrar la imagen dentro del botón */
+  justify-content: center;
+  align-items: center;
+  width: 100px; /* Ancho del botón */
+  height: 100px; /* Alto del botón */
+}
+
+/* Imagen de la casa */
+.casa-image {
+  max-width: 100%;
+  max-height: 100%;
+  width: auto; /* Mantiene proporciones */
+  height: auto; /* Mantiene proporciones */
+}
+
+a {
+  font-weight: 500;
+  color: #646cff;
+  text-decoration: inherit;
+}
+a:hover {
+  color: #535bf2;
+}
+
+
+#my-wrapper {
+  margin: auto;
+  max-width: 1280px;
+}
+
+#header {
+  max-width: 800px;
+  text-align: center;
+  margin: 50px auto;
+}
+#main_title {
+  font-size: 3em;
+  line-height: 1.1;
+}
+
+.subtitulo {
+  font-size: 1.5em;
+  text-align: center;
+}
+.texto_container {
+  max-width: 700px;
+  padding: 0 20px;
+  margin: auto;
+  margin-bottom: 100px;
+  text-align: left;
+}
+.texto_parrafo {
+  margin: 50px auto;
+}
+
+</style>
